47b6d2746d2154bcddcee545a5c9406680a23b4d
Sergey Gusachenko <sgusachenkoq@gmail.com>
2020-02-11 17:54:41 -0800

    add stack view training project

---
+++ StackViewProject/StackViewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewProject
+//
+//  Created by Сергей Гусаченко on 2/11/20.
+//  Copyright © 2020 SergeiGusachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
