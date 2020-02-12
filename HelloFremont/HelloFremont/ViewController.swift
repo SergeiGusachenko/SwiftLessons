4c7e469e416fa2459ff4c888b0ff3c569f602d85
Sergey Gusachenko <sgusachenkoq@gmail.com>
2020-02-11 16:12:38 -0800

    all about auto layout

---
+++ HelloFremont/HelloFremont/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloFremont
+//
+//  Created by Сергей Гусаченко on 2/10/20.
+//  Copyright © 2020 SergeiGusachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello, Fremont!\n")
+    }
+
+
+}
+
